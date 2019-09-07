92750eeaee3c2d209e5061ce7bbce4c04bca8193
clh910936 <carrielhunner@gmail.com>
2019-09-07 15:03:45 -0400

    got the outline of the opcode mux setup

--- alu.v
+++ alu.v
@@ -5,6 +5,11 @@ input [4:0] ctrl_ALUopcode, ctrl_shiftamt;
 output [31:0] data_result;
 output isNotEqual, isLessThan, overflow;
 
-full_adder adder(data_operandA, data_operandB, ctrl_ALUopcode[0], data_result, overflow);
+wire [31:0] adderSum;
+
+full_adder adder(data_operandA, data_operandB, ctrl_ALUopcode[0], adderSum, overflow);
+
+mux_8 opcodeMux(ctrl_ALUopcode[2:0], addersum, addersum, addersum, addersum, addersum, addersum, addersum, addersum, data_result);
+
 
 endmodule
